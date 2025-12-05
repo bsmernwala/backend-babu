d21e593d1da91fe8cd3c04835ab6ab995a2a6853
bsmernwala <bsmernwala@gamil.com>
2025-12-05 20:31:32 +0530

    new commit done

--- DB.js
+++ DB.js
@@ -1,7 +1,7 @@
 // DB.js
 module.exports = {
     
-    URL:"mongodb+srv://bsmernwala_db_user:POOmV6l3ErS0nbBK@cluster0.r2chbyu.mongodb.net/ecommerceDB?retryWrites=true&w=majority&appName=Cluster0"
-
+    // URL:"mongodb+srv://bsmernwala_db_user:POOmV6l3ErS0nbBK@cluster0.r2chbyu.mongodb.net/ecommerceDB?retryWrites=true&w=majority&appName=Cluster0"
+uRL:"mongodb+srv://bsmernwala_db_user:B4dw5gG0iDZgQ9Za@cluster0.7mdrclv.mongodb.net/admin?authSource=admin&replicaSet=atlas-11wnbc-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
 }
 
